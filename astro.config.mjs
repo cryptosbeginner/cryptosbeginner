d39502da0eaf1b490b9890fdc188a94b2247a3bb
Cryptos Beginner <taimoorkibria@outlook.com>
2026-08-16 23:11:45 +0000

    Switch to pure static output to fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'hybrid',
-  adapter: vercel(),
+  output: 'static',
   integrations: [tailwind()],
 });
